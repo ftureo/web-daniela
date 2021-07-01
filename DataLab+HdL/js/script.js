eebcc64f90324fc08ebe63992f42b5a4079420f1
Fabián Tureo <fabianestebantureo@gmail.com>
2021-07-01 17:34:04 -0300

    Update repository

---
+++ DataLab+HdL/js/script.js
@@ -0,0 +1,8 @@
+function toggleClass() {
+    var trailer = document.querySelector('.trailer')
+    var video = document.querySelector('video')
+    trailer.classList.toggle('active')
+    video.pause()
+    video.currentTime = 0
+
+}
